69c5f1105fce93cabdcb047882c5d35aa636b4f1
Daniel <daniel.her@onja.org>
2020-09-11 10:36:06 +0300

    Changed name attribute into id

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-
+// Grab element from the html
 const images = Array.from(document.querySelectorAll(".small-image"));
-const figure = document.querySelector('figure');
 const currentImage = document.querySelector('picture');
-console.log(figure);
+console.log(currentImage);
 
+// Add event on each image
 images.forEach(image => image.addEventListener('click', (e) => {
+  // Create a block of html
   const html = `
-    <picture class="details_content-figure-picture active-picture">
-      <source srcset="./images/${e.target.name}_lg.jpg" class="image-active" media="(min-width: 1200px)">
-      <source srcset="./images/${e.target.name}_md.jpg" class="image-active" media="(min-width: 640px)">
-      <img src="./images/${e.target.name}_sm.jpg" alt="${e.target.alt}" class="image-active" />
-    </picture>
+      <source srcset="./images/${e.target.id}_lg.jpg" class="image-active" media="(min-width: 1200px)">
+      <source srcset="./images/${e.target.id}_md.jpg" class="image-active" media="(min-width: 640px)">
+      <img src="./images/${e.target.id}_sm.jpg" alt="${e.target.alt}" class="image-active" />
   `;
 
+// Condition for active image
   if (e.target.classList.contains('active-small')){
         e.target.classList.remove('active-small');
         e.target.classList.add('not-active');
-        currentImage.innerHTML = html;
+        return currentImage;
+// Condition for the deactivated image
   } else if (e.target.classList.contains('not-active')) {
         e.target.classList.remove('not-active');
         e.target.classList.add('active-small')
